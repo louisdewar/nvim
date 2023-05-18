eccde5c7247f074d40d96435571fdf423722d824
Louis de Wardt <louis.dewardt@passfort.com>
2023-05-18 08:32:31 +0100

    fix treesitter config

--- lua/ldw/plugins/treesitter.lua
+++ lua/ldw/plugins/treesitter.lua
@@ -20,27 +20,27 @@ return {
         "javascript",
         "markdown",
         "markdown_inline",
-      }
+      },
+      highlight = {
+        enable = true,
+      },
+      indent = {
+        enable = true,
+        disable = { "python" },
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = "<nop>",
+          node_decremental = "<bs>",
+        },
+      },
     },
     keys = {
       { "<c-space>", desc = "Increment selection" },
-      { "<bs>", desc = "Schrink selection", mode = "x" },
-    },
-    highlight = {
-      enable = true,
-    },
-    indent = {
-      enable = true,
-      disable = { "python" },
-    },
-    incremental_selection = {
-      enable = true,
-      keymaps = {
-        init_selection = "<C-space>",
-        node_incremental = "<C-space>",
-        scope_incremental = "<nop>",
-        node_decremental = "<bs>",
-      },
+      { "<bs>",      desc = "Schrink selection",  mode = "x" },
     },
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
