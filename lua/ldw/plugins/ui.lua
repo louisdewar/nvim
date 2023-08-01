b85762af0bc8bc007248d079abf54a37aca04acc
Louis de Wardt <contact@louis.dewardt.uk>
2023-08-01 22:32:47 +0100

    add lualine

--- lua/ldw/plugins/ui.lua
+++ lua/ldw/plugins/ui.lua
@@ -7,7 +7,6 @@ return {
       vim.cmd([[colorscheme tokyonight-night]])
     end,
   },
-
   -- {
   --   "lukas-reineke/indent-blankline.nvim",
   --   event = { "BufReadPre", "BufNewFile" },
@@ -16,11 +15,17 @@ return {
   --     show_current_context_start = true,
   --   },
   -- },
-
   -- icons
   { "nvim-tree/nvim-web-devicons", lazy = true },
   -- ui components
-  { "MunifTanjim/nui.nvim",        lazy = true },
-
-  { "rcarriga/nvim-notify",        lazy = false }
+  { "MunifTanjim/nui.nvim", lazy = true },
+  { "rcarriga/nvim-notify", lazy = false },
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    dependencies = {
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {},
+  },
 }
