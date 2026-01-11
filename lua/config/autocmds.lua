f03567e6905a53086cbc90bfe202bda26b47ebdf
Louis de Wardt <contact@louis.dewardt.uk>
2026-01-11 10:05:36 +0000

    Automatically reload buffers
    This helps when doing <leader>jjl and then selecting a commit. Despite
    not actually "entering" the buffer, as soon as I select a commit in the
    popup, it immediately updates the background buffer.

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank { higroup = 'IncSearch', timeout = 1000 }
   end,
 })
+
+vim.api.nvim_create_autocmd('BufEnter', {
+  group = vim.api.nvim_create_augroup('checktime_on_bufenter', {}),
+  desc = 'Check if buffer changed on disk when entering',
+  command = 'checktime %',
+})
